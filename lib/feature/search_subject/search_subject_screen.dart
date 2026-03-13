6b95fa56f78a7c8b75e77d2cb465ddb4ccff5f86
Kanta Oikawa <iam@kantacky.com>
2026-03-13 15:53:18 +0900

    Make SearchSubjectScreen as a HookConsumerWidget

--- lib/feature/search_subject/search_subject_screen.dart
+++ lib/feature/search_subject/search_subject_screen.dart
@@ -4,12 +4,13 @@ import 'package:dotto/feature/search_subject/search_subject_filter_screen.dart';
 import 'package:dotto_design_system/component/text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SearchSubjectScreen extends HookWidget {
+class SearchSubjectScreen extends HookConsumerWidget {
   const SearchSubjectScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final textEditingController = useTextEditingController();
     final focusNode = useFocusNode();
     final subjects = useState<List<SubjectSummary>>([]);
