8090e35c79681de7ef7670e398c9694b055eb39f
Kanta Oikawa <iam@kantacky.com>
2026-03-13 14:44:25 +0900

    Modify AcademicArea

--- lib/domain/academic_area.dart
+++ lib/domain/academic_area.dart
@@ -1,38 +1,55 @@
 import 'package:collection/collection.dart';
 
 enum AcademicArea {
-  informationSystemCourse(
-    label: '情報システムコース',
-    deprecatedUserPreferenceKey: '情報システム',
-    deprecatedFilterOptionChoiceKey: '情報システムコース',
-  ),
-  informationDesignCourse(
-    label: '情報デザインコース',
-    deprecatedUserPreferenceKey: '情報デザイン',
-    deprecatedFilterOptionChoiceKey: '情報デザインコース',
-  ),
-  complexCourse(label: '複雑系コース', deprecatedUserPreferenceKey: '複雑', deprecatedFilterOptionChoiceKey: '複雑系コース'),
-  intelligenceSystemCourse(
-    label: '知能システムコース',
-    deprecatedUserPreferenceKey: '知能',
-    deprecatedFilterOptionChoiceKey: '知能システムコース',
-  ),
-  advancedICTCourse(
-    label: '高度ICTコース',
-    deprecatedUserPreferenceKey: '高度ICT',
-    deprecatedFilterOptionChoiceKey: '高度ICTコース',
-  ),
-  informationArchitectureArea(label: '情報アーキテクチャ領域'),
-  mediaDesignArea(label: 'メディアデザイン領域'),
-  complexInformationScienceArea(label: '複雑系情報科学領域'),
-  intelligenceInformationScienceArea(label: '知能情報科学領域'),
-  advancedICTArea(label: '高度ICT領域');
+  // 学部コース
+  informationSystemCourse,
+  informationDesignCourse,
+  complexCourse,
+  intelligenceSystemCourse,
+  advancedICTCourse,
+  // 大学院領域
+  informationArchitectureArea,
+  mediaDesignArea,
+  complexInformationScienceArea,
+  intelligenceInformationScienceArea,
+  advancedICTArea;
 
-  const AcademicArea({required this.label, this.deprecatedUserPreferenceKey, this.deprecatedFilterOptionChoiceKey});
-
-  final String label;
-  final String? deprecatedUserPreferenceKey;
-  final String? deprecatedFilterOptionChoiceKey;
+  String get label => switch (this) {
+    AcademicArea.informationSystemCourse => '情報システムコース',
+    AcademicArea.informationDesignCourse => '情報デザインコース',
+    AcademicArea.complexCourse => '複雑系コース',
+    AcademicArea.intelligenceSystemCourse => '知能システムコース',
+    AcademicArea.advancedICTCourse => '高度ICTコース',
+    AcademicArea.informationArchitectureArea => '情報アーキテクチャ領域',
+    AcademicArea.mediaDesignArea => 'メディアデザイン領域',
+    AcademicArea.complexInformationScienceArea => '複雑系情報科学領域',
+    AcademicArea.intelligenceInformationScienceArea => '知能情報科学領域',
+    AcademicArea.advancedICTArea => '高度ICT領域',
+  };
+  String? get deprecatedUserPreferenceKey => switch (this) {
+    AcademicArea.informationSystemCourse => '情報システム',
+    AcademicArea.informationDesignCourse => '情報デザイン',
+    AcademicArea.complexCourse => '複雑',
+    AcademicArea.intelligenceSystemCourse => '知能',
+    AcademicArea.advancedICTCourse => '高度ICT',
+    AcademicArea.informationArchitectureArea => null,
+    AcademicArea.mediaDesignArea => null,
+    AcademicArea.complexInformationScienceArea => null,
+    AcademicArea.intelligenceInformationScienceArea => null,
+    AcademicArea.advancedICTArea => null,
+  };
+  String? get deprecatedFilterOptionChoiceKey => switch (this) {
+    AcademicArea.informationSystemCourse => '情報システムコース',
+    AcademicArea.informationDesignCourse => '情報デザインコース',
+    AcademicArea.complexCourse => '複雑系コース',
+    AcademicArea.intelligenceSystemCourse => '知能システムコース',
+    AcademicArea.advancedICTCourse => '高度ICTコース',
+    AcademicArea.informationArchitectureArea => null,
+    AcademicArea.mediaDesignArea => null,
+    AcademicArea.complexInformationScienceArea => null,
+    AcademicArea.intelligenceInformationScienceArea => null,
+    AcademicArea.advancedICTArea => null,
+  };
 
   static AcademicArea? fromDeprecatedUserPreferenceKey(String key) {
     return values.firstWhereOrNull((e) => e.deprecatedUserPreferenceKey == key);
