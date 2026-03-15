5a8e7a8a78ae01beebbf3323cac776c66d378325
tanakarinna <abomasnow0460@gmail.com>
2026-03-15 18:21:10 +0900

    Add クラスの骨組みを作成

--- lib/feature/setting/widget/user_info_tile.dart
+++ lib/feature/setting/widget/user_info_tile.dart
@@ -2,3 +2,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:dotto_design_system/style/semantic_color.dart';
 import 'package:dotto/asset.dart';
+
+final class UserInfoTile extends StatelessWidget {
+  const UserInfoTile({super.key, required this.user});
+
+  /// 表示するユーザー（Firebase Auth の User）
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(); // 仮
+  }
+}
