efb6ab386a86ab9197ca5c57332113d90f80a90f
tanakarinna <abomasnow0460@gmail.com>
2026-03-15 18:04:24 +0900

    Add import

---
+++ lib/feature/setting/widget/user_info_tile.dart
@@ -0,0 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:dotto_design_system/style/semantic_color.dart';
+import 'package:dotto/asset.dart';
