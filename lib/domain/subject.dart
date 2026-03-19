abfe0fabc46499d22e8bd7add6203cb9eba5a98a
Kanta Oikawa <iam@kantacky.com>
2026-03-19 17:27:20 +0900

    Add Subject

---
+++ lib/domain/subject.dart
@@ -0,0 +1,23 @@
+import 'package:dotto/domain/semester.dart';
+import 'package:dotto/domain/subject_eligible_attribute.dart';
+import 'package:dotto/domain/subject_faculty.dart';
+import 'package:dotto/domain/subject_requirement.dart';
+import 'package:dotto/domain/syllabus.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'subject.freezed.dart';
+
+@freezed
+abstract class Subject with _$Subject {
+  const factory Subject({
+    required String id,
+    required String name,
+    required List<SubjectFaculty> faculties,
+    required int year,
+    required Semester semester,
+    required int credit,
+    required List<SubjectEligibleAttribute> eligibleAttributes,
+    required List<SubjectRequirement> requirements,
+    required Syllabus syllabus,
+  }) = _Subject;
+}
