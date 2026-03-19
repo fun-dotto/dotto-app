e4867aba8d7519bbd9fce259280977240b9a4902
Kanta Oikawa <iam@kantacky.com>
2026-03-19 19:51:34 +0900

    大学院の領域を削除して学部と共通化

--- lib/domain/academic_area.dart
+++ lib/domain/academic_area.dart
@@ -1,30 +1,18 @@
 import 'package:collection/collection.dart';
 
 enum AcademicArea {
-  // 学部コース
   informationSystemCourse,
   informationDesignCourse,
   complexCourse,
   intelligenceSystemCourse,
-  advancedICTCourse,
-  // 大学院領域
-  informationArchitectureArea,
-  mediaDesignArea,
-  complexInformationScienceArea,
-  intelligenceInformationScienceArea,
-  advancedICTArea;
+  advancedICTCourse;
 
   String get label => switch (this) {
-    AcademicArea.informationSystemCourse => '情報システムコース',
-    AcademicArea.informationDesignCourse => '情報デザインコース',
-    AcademicArea.complexCourse => '複雑系コース',
-    AcademicArea.intelligenceSystemCourse => '知能システムコース',
-    AcademicArea.advancedICTCourse => '高度ICTコース',
-    AcademicArea.informationArchitectureArea => '情報アーキテクチャ領域',
-    AcademicArea.mediaDesignArea => 'メディアデザイン領域',
-    AcademicArea.complexInformationScienceArea => '複雑系情報科学領域',
-    AcademicArea.intelligenceInformationScienceArea => '知能情報科学領域',
-    AcademicArea.advancedICTArea => '高度ICT領域',
+    AcademicArea.informationSystemCourse => '情報システム/情報アーキテクチャ',
+    AcademicArea.informationDesignCourse => '情報デザイン/メディアデザイン',
+    AcademicArea.complexCourse => '複雑系/複雑系情報科学',
+    AcademicArea.intelligenceSystemCourse => '知能システム/知能情報科学',
+    AcademicArea.advancedICTCourse => '高度ICT',
   };
   String? get deprecatedUserPreferenceKey => switch (this) {
     AcademicArea.informationSystemCourse => '情報システム',
@@ -32,11 +20,6 @@ enum AcademicArea {
     AcademicArea.complexCourse => '複雑',
     AcademicArea.intelligenceSystemCourse => '知能',
     AcademicArea.advancedICTCourse => '高度ICT',
-    AcademicArea.informationArchitectureArea => null,
-    AcademicArea.mediaDesignArea => null,
-    AcademicArea.complexInformationScienceArea => null,
-    AcademicArea.intelligenceInformationScienceArea => null,
-    AcademicArea.advancedICTArea => null,
   };
   String? get deprecatedFilterOptionChoiceKey => switch (this) {
     AcademicArea.informationSystemCourse => '情報システムコース',
@@ -44,11 +27,6 @@ enum AcademicArea {
     AcademicArea.complexCourse => '複雑系コース',
     AcademicArea.intelligenceSystemCourse => '知能システムコース',
     AcademicArea.advancedICTCourse => '高度ICTコース',
-    AcademicArea.informationArchitectureArea => null,
-    AcademicArea.mediaDesignArea => null,
-    AcademicArea.complexInformationScienceArea => null,
-    AcademicArea.intelligenceInformationScienceArea => null,
-    AcademicArea.advancedICTArea => null,
   };
 
   static AcademicArea? fromDeprecatedUserPreferenceKey(String key) {
