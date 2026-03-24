16a0e1ed3e6a020aa40453c155c41b706366dc74
Kanta Oikawa <iam@kantacky.com>
2026-03-24 21:15:29 +0900

    Add PersonalTimetableCalendarView

---
+++ lib/feature/course_registration/personal_timetable_calendar_view.dart
@@ -0,0 +1,24 @@
+import 'package:dotto/api/api_client.dart';
+import 'package:dotto/domain/subject_summary.dart';
+import 'package:dotto/feature/course_registration/course_registration_repository.dart';
+import 'package:dotto/feature/course_registration/personal_timetable_calendar_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final class PersonalTimetableCalendarView extends HookConsumerWidget {
+  const PersonalTimetableCalendarView({required this.onSubjectSelected, super.key});
+
+  final void Function(SubjectSummary) onSubjectSelected;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final apiClient = ref.read(apiClientProvider);
+    final courseRegistrationRepository = CourseRegistrationRepositoryImpl(apiClient);
+    final courseRegistrationService = PersonalTimetableCalendarService(courseRegistrationRepository);
+    final personalTimetableItems = useFuture(useMemoized(() => courseRegistrationService.getPersonalTimetableItems()));
+
+    // TODO
+    return const SizedBox.shrink();
+  }
+}
