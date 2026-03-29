160a4886f26d3906d51393942b924882f9569dbf
Kanta Oikawa <iam@kantacky.com>
2026-03-29 20:52:56 +0900

    Revert "認証インターセプターでIDトークン取得待機を追加"
    This reverts commit a56660fb97a6f09dd5368e18ca1734704816844f.

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:dio/dio.dart';
 import 'package:dotto/api/api_environment.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
@@ -7,33 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:openapi/openapi.dart';
-
-Future<String?> _getIdTokenForRequest() async {
-  final currentToken = await FirebaseAuth.instance.currentUser?.getIdToken();
-  if (currentToken != null && currentToken.isNotEmpty) {
-    return currentToken;
-  }
-
-  try {
-    final nextUser = await FirebaseAuth.instance
-        .idTokenChanges()
-        .firstWhere((user) => user != null)
-        .timeout(const Duration(seconds: 2));
-    final nextToken = await nextUser?.getIdToken();
-    if (nextToken != null && nextToken.isNotEmpty) {
-      return nextToken;
-    }
-  } on TimeoutException {
-    debugPrint('ID token wait timed out');
-  }
-
-  final refreshedToken = await FirebaseAuth.instance.currentUser?.getIdToken(true);
-  if (refreshedToken != null && refreshedToken.isNotEmpty) {
-    return refreshedToken;
-  }
-
-  return null;
-}
 
 final apiClientProvider = Provider<Openapi>(
   (ref) => Openapi(
@@ -46,7 +17,7 @@ final apiClientProvider = Provider<Openapi>(
           if (appCheckToken != null) {
             options.headers['X-Firebase-AppCheck'] = 'Bearer $appCheckToken';
           }
-          final idToken = await _getIdTokenForRequest();
+          final idToken = await FirebaseAuth.instance.currentUser?.getIdToken();
           if (idToken != null) {
             options.headers['Authorization'] = 'Bearer $idToken';
           }
