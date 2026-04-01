de3dddf03862c7bf4df5b7807a010f6c7ff2319f
Kanta Oikawa <iam@kantacky.com>
2026-04-01 16:24:53 +0900

    設定項目をログイン時のみ表示する

--- lib/feature/search_subject_v0/search_course_usecase.dart
+++ lib/feature/search_subject_v0/search_course_usecase.dart
@@ -1,7 +1,5 @@
 import 'package:dotto/domain/academic_area.dart';
 import 'package:dotto/domain/grade.dart';
-import 'package:dotto/domain/user_preference_keys.dart';
-import 'package:dotto/helper/user_preference_repository.dart';
 
 final class SearchCourseUsecase {
   factory SearchCourseUsecase() {
@@ -11,12 +9,10 @@ final class SearchCourseUsecase {
   static final SearchCourseUsecase _instance = SearchCourseUsecase._internal();
 
   Future<Grade?> getUserGrade() async {
-    final deprecatedGradeKey = await UserPreferenceRepository.getString(UserPreferenceKeys.grade);
-    return Grade.fromDeprecatedUserPreferenceKey(deprecatedGradeKey ?? '');
+    return null;
   }
 
   Future<AcademicArea?> getUserAcademicArea() async {
-    final deprecatedAcademicAreaKey = await UserPreferenceRepository.getString(UserPreferenceKeys.course);
-    return AcademicArea.fromDeprecatedUserPreferenceKey(deprecatedAcademicAreaKey ?? '');
+    return null;
   }
 }
