711a3597c400b5e9bbf12a5ca86ffa98c94866a1
Kanta Oikawa <iam@kantacky.com>
2026-04-11 12:09:13 +0900

    APIEnvironmentを定義

---
+++ Shared/Sources/DottoModel/APIEnvironment.swift
@@ -0,0 +1,13 @@
+//
+//  APIEnvironment.swift
+//  Shared
+//
+//  Created by Kanta Oikawa on 2026/04/11.
+//
+
+public enum APIEnvironment {
+    case prod
+    case qa
+    case stg
+    case dev
+}
