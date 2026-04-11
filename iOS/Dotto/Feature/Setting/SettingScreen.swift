d2733a276c427a8d488b2e080748c13660c6822d
Kanta Oikawa <iam@kantacky.com>
2026-04-12 00:26:45 +0900

    お知らせを設定画面に追加

--- iOS/Dotto/Feature/Setting/SettingScreen.swift
+++ iOS/Dotto/Feature/Setting/SettingScreen.swift
@@ -20,22 +20,36 @@ struct SettingScreen: View {
     var body: some View {
         NavigationStack {
             List {
-                if let user = store.user {
-                    UserView(user: user)
-                    Button("Sign Out") {
-                        store.send(.onSignOutButtonTapped)
-                    }
-                } else {
-                    Button("Sign In") {
-                        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                              let viewController = windowScene.windows.first?.rootViewController else {
-                            return
+                Section {
+                    if let user = store.user {
+                        UserView(user: user)
+                        Button("Sign Out") {
+                            store.send(.onSignOutButtonTapped)
                         }
-                        store.send(.onSignInButtonTapped(viewController: viewController))
+                    } else {
+                        Button("Sign In") {
+                            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                                  let viewController = windowScene.windows.first?.rootViewController else {
+                                return
+                            }
+                            store.send(.onSignInButtonTapped(viewController: viewController))
+                        }
+                    }
+                }
+
+                Section {
+                    NavigationLink {
+                        AnnouncementScreen(
+                            store: Store(initialState: .init()) {
+                                AnnouncementFeature()
+                            }
+                        )
+                    } label: {
+                        Label("お知らせ", systemImage: "bell")
                     }
                 }
             }
-            .navigationTitle("Setting")
+            .navigationTitle("Settings")
         }
         .onAppear {
             store.send(.onAppear)
