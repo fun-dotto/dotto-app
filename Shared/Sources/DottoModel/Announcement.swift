41a39b5674ad229446ede5a5d289903d6fcc4e0d
Kanta Oikawa <iam@kantacky.com>
2026-04-11 12:09:22 +0900

    Announcementモデルを定義

---
+++ Shared/Sources/DottoModel/Announcement.swift
@@ -0,0 +1,22 @@
+//
+//  Announcement.swift
+//  Shared
+//
+//  Created by Kanta Oikawa on 2026/04/11.
+//
+
+import Foundation
+
+public struct Announcement: Hashable, Sendable {
+    public let id: String
+    public let title: String
+    public let date: Date
+    public let url: URL
+
+    public init(id: String, title: String, date: Date, url: URL) {
+        self.id = id
+        self.title = title
+        self.date = date
+        self.url = url
+    }
+}
