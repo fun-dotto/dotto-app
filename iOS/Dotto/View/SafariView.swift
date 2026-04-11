71e3e44874ceb5f34cee04ac880b8d0bae41f0be
Kanta Oikawa <iam@kantacky.com>
2026-04-12 00:19:09 +0900

    Add SafariView

---
+++ iOS/Dotto/View/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  Dotto
+//
+//  Created by Kanta Oikawa on 2026/04/12.
+//
+
+import SafariServices
+import SwiftUI
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+    }
+}
