025d6056af8315c81ccd9aa165e6abcd40e4c58f
Kanta Oikawa <iam@kantacky.com>
2026-04-14 19:53:50 +0900

    Add shared package to settings.gradle.kts

--- Android/settings.gradle.kts
+++ Android/settings.gradle.kts
@@ -19,8 +19,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
     }
 }
 
 rootProject.name = "Dotto"
 include(":app")
+
+include(":swift-java-dotto-shared")
+project(":swift-java-dotto-shared").projectDir = file("../Shared")
