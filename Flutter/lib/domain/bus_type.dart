98d2586f6c1fea433e3acd799adfbcde4a9e80c7
Kanta Oikawa <iam@kantacky.com>
2026-05-03 16:55:26 +0900

    BusLandmarkのlabelから方面を削除

--- Flutter/lib/domain/bus_type.dart
+++ Flutter/lib/domain/bus_type.dart
@@ -1,7 +1,7 @@
 enum BusLandmark {
   kameda(label: '亀田支所'),
-  goryokaku(label: '五稜郭方面'),
-  syowa(label: '昭和方面'),
+  goryokaku(label: '五稜郭'),
+  syowa(label: '昭和'),
   other;
 
   const BusLandmark({this.label});
