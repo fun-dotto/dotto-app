78941ccda816e2ca2d247e4e7b5d895d8ed6ce42
Kanta Oikawa <iam@kantacky.com>
2026-05-03 16:54:02 +0900

    BusTypeのlabelプロパティをオプショナルに変更

--- Flutter/lib/domain/bus_type.dart
+++ Flutter/lib/domain/bus_type.dart
@@ -1,10 +1,10 @@
 enum BusType {
-  kameda('亀田支所'),
-  goryokaku('五稜郭方面'),
-  syowa('昭和方面'),
-  other('');
+  kameda(label: '亀田支所'),
+  goryokaku(label: '五稜郭方面'),
+  syowa(label: '昭和方面'),
+  other;
 
-  const BusType(this.label);
+  const BusType({this.label});
 
-  final String label;
+  final String? label;
 }
